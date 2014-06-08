6fe43054366473e3d8f091be3f5ca6d8d8fbf7cc
yogaII4git <yogaII@gmail.com>
2014-06-08 15:57:53 -0400

    Plot2
    and fix a bug in plot1 R file

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-#Assignment 1 plot 1
+#Assignment 1 plot 2
 
 #Load file and filter data first
 
@@ -6,8 +6,11 @@ alldata <- read.table("household_power_consumption.txt", header = TRUE, sep = ";
 mydata = alldata[which(as.Date(alldata[,'Date'], "%d/%m/%Y") == "2007-02-01" | as.Date(alldata[,'Date'], "%d/%m/%Y") == "2007-02-02"), ]
 
 #Now lets do some plotting
-hist(test[, 3], main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)")
+plot(mydata[, 3], type = "l", ylab = "Global Active Power (kilowatts)", xaxt = "n", xlab="")
+#Here I am hardcoding the weekday info. Ideally, I would like to write util functions to do such
+axis(1, at=c(1, 1441, 2881), lab = c("Thu","Fri","Sat"))
+
 
 #Now copy to png
-dev.copy(png, 'plot1.png')
+dev.copy(png, 'plot2.png')
 dev.off()
